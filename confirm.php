4920302d245f3268d8302f43c0e18564bf612cb7
Shintaroh Itakura <shintaroitakura@gmail.com>
2018-05-27 12:21:11 -0700

    check out button confirmation email update

--- confirm.php
+++ confirm.php
@@ -1,52 +1,48 @@
+<form action="confirm.php" method="post">
+<label>Subject of email:</label><br>
+<input type="text" name="subject" id="subject"/><br>
+<label>Body of email:</label><br>
+<textarea name="body" id="body" rows="10" cols="35"></textarea><br>
+<input type="submit" name=submit value="Submit"/>
+</form>
 <?php
 // testing git update with this text
 //setup some variables
-$action = array();
-$action['result'] = null;
- 
-//quick/simple validation
-if(empty($_GET['email']) || empty($_GET['key'])){
-    $action['result'] = 'error';
-    $action['text'] = 'We are missing variables. Please double check your email.';          
+// enter username password hostname database name table name to connect to mySQL server table
+$user = "user_name"; 
+$password = "password"; 
+$host = "host_name"; 
+$dbase = "database_name"; 
+$table = "table_name"; 
+
+//email address from where you want to send order confirmation from
+$from= 'email_address';
+
+//retrieves subject title and body 
+$subject= $_POST['subject'];
+$body= $_POST['body'];
+
+// connect to mySQL server
+$dbc= mysqli_connect($host,$user,$password, $dbase) 
+or die("Unable to select database");
+
+//mySQL query to retrieve order data
+$query= "SELECT * FROM $table";
+$result= mysqli_query ($dbc, $query) 
+or die ('Error querying database.');
+
+//fetches email list from database
+while ($row = mysqli_fetch_array($result)) {
+$first_name= $row['first_name'];
+$last_name= $row['last_name'];
+$email= $row['email'];
+
+//write email
+$msg= "Dear $first_name $last_name,\n$body";
+mail($email, $subject, $msg, 'From:' . $from);
+echo 'Email sent to: ' . $email. '<br>';
 }
-         
-if($action['result'] != 'error'){
- 
-    //cleanup the variables
-    $email = mysql_real_escape_string($_GET['email']);
-    $key = mysql_real_escape_string($_GET['key']);
-     
-    //check if the key is in the database
-    $check_key = mysql_query("SELECT * FROM `confirm` WHERE `email` = '$email' AND `key` = '$key' LIMIT 1") or die(mysql_error());
-     
-    if(mysql_num_rows($check_key) != 0){
-                 
-        //get the confirm info
-        $confirm_info = mysql_fetch_assoc($check_key);
-         
-        //confirm the email and update the users database
-        $update_users = mysql_query("UPDATE `users` SET `active` = 1 WHERE `id` = '$confirm_info[userid]' LIMIT 1") or die(mysql_error());
-        //delete the confirm row
-        $delete = mysql_query("DELETE FROM `confirm` WHERE `id` = '$confirm_info[id]' LIMIT 1") or die(mysql_error());
-         
-        if($update_users){
-                         
-            $action['result'] = 'success';
-            $action['text'] = 'User has been confirmed. Thank-You!';
-         
-        }else{
- 
-            $action['result'] = 'error';
-            $action['text'] = 'The user could not be updated Reason: '.mysql_error();;
-         
-        }
-     
-    }else{
-     
-        $action['result'] = 'error';
-        $action['text'] = 'The key and email is not in our database.';
-     
-    }
- 
-}
+
+//close database
+mysqli_close($dbc);
 ?>
