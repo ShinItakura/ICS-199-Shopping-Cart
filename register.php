d766a3552c5ca72ce9ddbadfdba79ca83eb4dff1
Shintaroh Itakura <shintaroitakura@gmail.com>
2018-05-27 15:00:02 -0700

    altered code and query to work with new database, and added in line of code to handle database check for existing data (not active commented out at this time).

--- register.php
+++ register.php
@@ -14,27 +14,7 @@
       <br/>
       Password: <input type="password" name="password">
       <br/>
-      Street Address: <input type="text" name="street">
-      <br/>
-      Apt Number: <input type="number" name="apt">
-      <br/>
-      City: <input type="text" name="city">
-      <br/>
-      Province: <select name="province">
-        <option value="AB">Alberta</option> 
-        <option value="BC">British Columbia</option>
-        <option value="MB">Manitoba</option>
-        <option value="NB">New Brunswick</option>
-        <option value="NL">Newfoundland</option>
-        <option value="NS">Nova Scotia</option>
-        <option value="NT">Northwest Territories</option>
-        <option value="NU">Nunavut</option>
-        <option value="ON">Ontario</option>
-        <option value="PE">PEI</option>
-        <option value="QC">Quebec</option>
-        <option value="SK">Saskatchewan</option>
-        <option value="YT">Yukon</option>
-      </select>
+      Address: <input type="text" name="street">
       <br/>
       Country: <select name="country">
         <option value="Canada">Canada</option>
@@ -48,6 +28,13 @@
     
     <?php
       ini_set('display_errors',1);
+      /*function recordExists($table, $where, $mysqli{
+        $query = "SELECT * FROM `$table` WHERE $where;
+        $result = $mysqli->query($query);
+            if($result->num_rows > 0 {
+                echo "account already exists";
+            }
+      }*/
       if (isset($_POST['password'])) {
         $db = mysqli_connect('localhost', 'cst177', '461570', 'ICS199Group13_dev');
         mysqli_set_charset($db, 'utf8');
@@ -57,16 +44,9 @@
         $first = mysqli_real_escape_string($db, $_POST['first']);
         $last = mysqli_real_escape_string($db, $_POST['last']);
         $street = mysqli_real_escape_string($db, $_POST['street']);
-        $city = mysqli_real_escape_string($db, $_POST['city']);
         $country = mysqli_real_escape_string($db, $_POST['country']);
         $postcode = str_replace(' ', '', mysqli_real_escape_string($db, $_POST['postcode']));
-        $province = mysqli_real_escape_string($db, $_POST['province']);
-        if ($_POST['apt'] != '') {
-          $apt = (int) mysqli_real_escape_string($db, $_POST['apt']);
-        } else {
-          $apt = "NULL";
-        }
-        $query = "INSERT INTO USERS (email, password, first, last, street_address, city, country, postcode, province, apt_no) VALUES ('$email', '$pw_hash', '$first', '$last', '$street', '$city', '$country', '$postcode', '$province', $apt);";
+        $query = "INSERT INTO USER (fname, lname, password, email, address, country, postcode, role) VALUES ('$first', '$last', '$pw_hash', '$email', '$street', '$country', '$postcode', 'customer');";
         mysqli_query($db, $query) or die(mysqli_error($db));
         mysqli_close($db);
       }
