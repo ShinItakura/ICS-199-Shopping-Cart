5ca096d9529dc2c332ca26257d429874153494a8
Ken <kb_rw@hotmail.com>
2018-05-29 15:13:43 -0700

    Week 3 demo versions

--- addproductresult.php
+++ addproductresult.php
@@ -15,6 +15,40 @@
 	$manu = $_POST['manufacturer'];
 	$axle = $_POST['axle'];
 	
+	//if (move_uploaded_file ($_FILES['the_file']['tmp_name'], "/home/student/cst107/myfiles/{$_FILES['the_file']['name']}")) {
+/*	if (move_uploaded_file ($_FILES['the_file']['tmp_name'], "/home/student/cst107/public_html/ICS199/project/{$_FILES['the_file']['name']}")) {
+	
+		print '<p>Your picture has been uploaded.</p>';
+	
+	} else { // Problem!
+		print '<p style="color: red;">Your file could not be uploaded because: ';
+		
+		// Print a message based upon the error:
+		switch ($_FILES['the_file']['error']) {
+			case 1:
+				print 'The file exceeds the upload_max_filesize setting in php.ini';
+				break;
+			case 2:
+				print 'The file exceeds the MAX_FILE_SIZE setting in the HTML form';
+				break;
+			case 3:
+				print 'The file was only partially uploaded';
+				break;
+			case 4:
+				print 'No file was uploaded';
+				break;
+			case 6:
+				print 'The temporary folder does not exist.';
+				break;
+			default:
+				print 'Something unforeseen happened.';
+				break;
+		}
+		
+		print '.</p>';
+	}	
+		*/
+	
 	echo "<p>$name</p>";
 	echo "<p>$desc</p>";
 	echo "<p>$$price</p>";
@@ -26,6 +60,8 @@
 	echo "<p>$axle</p>";
 	
 	$db = mysqli_connect('localhost', 'cst107','446287', 'ICS199Group13_dev');
+//	$validate = "select name from ITEM";
+//	$names = array	
 	$query = "insert into ITEM (name, AXLE_id, MATERIAL_id, SHAPE_id, price, image, color, manufacturer, description)
 	values('$name', '$axle', '$mat', '$shape', CAST('$price' AS DECIMAL(5,2)), '$pic', '$color', '$manu', '$desc');";
 	//NOW(), 
--- index.php
+++ index.php
@@ -1,41 +1,132 @@
+<?php
+session_start();
+$db_host = 'localhost'; // Server Name
+$db_user = 'cst107'; // Username
+$db_pass = '446287'; // Password
+$db_name = 'ICS199Group13_dev'; // Database Name
+
+$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+if (!$connect) {
+	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+?>
+<!doctype html>
 <html>
 <head>
-<title>Result</title>
+<meta charset="UTF-8">
+<title>Products - Yo Yo Ma's House of Yo-Yo's</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 </head>
 <body>
-<?php
 
-	$name = strip_tags($_POST['name']);
-	$desc = strip_tags($_POST['description']);
-	$price = strip_tags($_POST['price']);
-	$pic = strip_tags($_POST['picture']);
-	$shape = $_POST['shape'];
-	$color = $_POST['color'];
-	$mat = $_POST['material'];
-	$manu = $_POST['manufacturer'];
-	$axle = $_POST['axle'];
-	
-	echo "<p>$name</p>";
-	echo "<p>$desc</p>";
-	echo "<p>$$price</p>";
-	echo "<p>$pic</p>";
-	echo "<p>$shape</p>";
-	echo "<p>$color</p>";
-	echo "<p>$mat</p>";	
-	echo "<p>$manu</p>";	
-	echo "<p>$axle</p>";
-	
-	$db = mysqli_connect('localhost', 'cst107','446287', 'ICS199Group13_dev');
-	$query = "insert into ITEM (name, AXLE_id, MATERIAL_id, SHAPE_id, price, image, color, manufacturer, description)
-	values('$name', '$axle', '$mat', '$shape', CAST('$price' AS DECIMAL(5,2)), '$pic', '$color', '$manu', '$desc');";
-	//NOW(), 
-if (@mysqli_query($db, $query)) {
-	print '<p>The product has been successfully added.</p>';
-} else {
-	print '<p>The product could not be added because:<br>' . mysqli_error($db) . '.</p>';
-	print '<p>The query being run was: ' . $query . '</p>'; 
-}
+	<div class="container" style="width:60%;">
+	<h2 align="center">Yo Yo Ma's House of Yo-Yo's</h2>
+	<form action="index.php" method="GET">
+		<p>Axle: <select name="axle">
+			<option value="0">All</option>
+			<option value="1">Ball Bearing</option>
+			<option value="2">Fixed</option>
+			<option value="3">Transaxle</option>
+		</select> 
+		Shape: <select name="shape">
+			<option value="0">All</option>
+			<option value="1">Butterfly</option>
+			<option value="2">Classic</option>
+			<option value="3">Imperial</option>
+			<option value="4">Modified</option>
+		</select> 
+		<input type="submit" value="Submit"/>
+		</p>
+	</form>
 
-?>
-</body>
+    <?php
+//	ini_set('display_errors',1);
+	$axleChoice = $_GET['axle'];
+	$shapeChoice = $_GET['shape'];
+	$query ="SELECT * FROM ITEM";
+	$previous = false;
+	if ($axleChoice != 0) {
+	//	$query = "SELECT * FROM ITEM WHERE AXLE_id = $axleChoice";
+		$query .= " WHERE AXLE_id = $axleChoice";
+		$previous = true;
+	}
+	if ($shapeChoice != 0) {
+		if ($previous) {
+		//	$query = "SELECT * FROM ITEM WHERE AXLE_id = $axleChoice AND SHAPE_id = $shapeChoice";
+			$query .= " AND SHAPE_id = $shapeChoice";
+		} else {
+		//	$query = "SELECT * FROM ITEM WHERE SHAPE_id = $shapeChoice";
+			$query .= " WHERE SHAPE_id = $shapeChoice";
+		}
+	}
+	$result = mysqli_query($connect, $query);
+		
+	if(mysqli_num_rows($result) > 0)
+	{
+		while($row = mysqli_fetch_array($result))
+		{
+			?>
+            <div class="col-md-3">
+            <form method="post" action="shop.php?action=add&id=<?php echo $row["id"]; ?>">
+            <div style="border: 1px solid #eaeaec; margin: -1px 19px 3px -1px; box-shadow: 0 1px 2px rgba(0,0,0,0.05); padding:10px;" align="center">
+            <img src="<?php echo $row["image"]; ?>" class="img-responsive">
+            <h5 class="text-info"><?php echo $row["name"]; ?></h5>
+            <h5 class="text-danger">$ <?php echo $row["price"]; ?></h5>
+            <input type="text" name="quantity" class="form-control" value="1">
+            <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>">
+            <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>">
+            <input type="submit" name="add" style="margin-top:5px;" class="btn btn-default" value="Add to Bag">
+            </div>
+            </form>
+            </div>
+            <?php
+		}
+	}
+
+  // Shopping Cart
+	?>
+    <div style="clear:both"></div>
+    <h2>My Shopping Bag</h2>
+    <div class="table-responsive">
+    <table class="table table-bordered">
+    <tr>
+    <th width="40%">Product Name</th>
+    <th width="10%">Quantity</th>
+    <th width="20%">Price Details</th>
+    <th width="15%">Order Total</th>
+    <th width="5%">Action</th>
+    </tr>
+    <?php
+	if(!empty($_SESSION["cart"]))
+	{
+		$total = 0;
+		foreach($_SESSION["cart"] as $keys => $values)
+		{
+			?>
+            <tr>
+            <td><?php echo $values["item_name"]; ?></td>
+            <td><?php echo $values["item_quantity"] ?></td>
+            <td>$ <?php echo $values["product_price"]; ?></td>
+            <td>$ <?php echo number_format($values["item_quantity"] * $values["product_price"], 2); ?></td>
+            <td><a href="shop.php?action=delete&id=<?php echo $values["product_id"]; ?>"><span class="text-danger">X</span></a></td>
+            </tr>
+            <?php
+			$total = $total + ($values["item_quantity"] * $values["product_price"]);
+		}
+		?>
+        <tr>
+        <td colspan="3" align="right">Total</td>
+        <td align="right">$ <?php echo number_format($total, 2); ?></td>
+        <td></td>
+        </tr>
+        <?php
+	}
+	?>
+    </table>
+    </div>
+    </div>
+ </body>
 </html>
