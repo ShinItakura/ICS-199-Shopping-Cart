38d3cd7aa55af1a3097ef20d4551932c181bbbc4
shin <shinitakura@Shins-MacBook-Air.local>
2018-05-31 14:45:44 -0700

    added title and css

--- showaccount.php
+++ showaccount.php
@@ -1,3 +1,21 @@
+<head>
+    <title>My Account</title>
+    <style>
+            table {
+                font-family: arial, sans-serif;
+                border-collapse: collapse;
+                width: 100%;
+            }
+            td, th {
+                border: 1px solid #dddddd;
+                text-align: left;
+                padding: 8px;
+            }
+            tr:nth-child(even) {
+                background-color: #dddddd;
+            }
+        </style>
+</head>
 <?php 
 ini_set('display_errors',1);
 
