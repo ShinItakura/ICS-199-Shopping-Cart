e1cbec3c501ea1f960d322cff89fc24fb8764b2e
Paul Sajna <paulsajna@gmail.com>
2018-06-04 15:06:42 -0700

    revert bob's changes

--- header.php
+++ header.php
@@ -36,3 +36,6 @@
 			</div>
 		</div>
 
+<?php
+include 'nav.php';
+?>
--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
 session_start(); //start session
-
-
-include 'header.php';
-include 'nav.php';
-
 include('mysqli_connect.php');
 //include 'header.php';
 //include 'nav.php';
-
 
 ?>
 <!DOCTYPE HTML>
@@ -18,8 +12,84 @@ include('mysqli_connect.php');
 <title>Shopping Cart</title>
 <link href="style/style.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+<script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+<script>
+$(document).ready(function(){
+		$(".form-item").submit(function(e){
+			var form_data = $(this).serialize();
+			var button_content = $(this).find('button[type=submit]');
+			button_content.html('Adding...'); //Loading button text
+
+			$.ajax({ //make ajax request to cart_process.php
+				url: "cart_process.php",
+				type: "POST",
+				dataType:"json", //expect json value from server
+				data: form_data
+			}).done(function(data){ //on Ajax success
+				$("#cart-info").html(data.items); //total items in cart-info element
+				button_content.html('Add to Cart'); //reset button text to original text
+				alert("Item added to Cart!"); //alert user
+				if($(".shopping-cart-box").css("display") == "block"){ //if cart box is still visible
+					$(".cart-box").trigger( "click" ); //trigger click to update the cart box.
+				}
+			})
+			e.preventDefault();
+		});
+
+	//Show Items in Cart
+	$( ".cart-box").click(function(e) { //when user clicks on cart box
+		e.preventDefault();
+		$(".shopping-cart-box").fadeIn(); //display cart box
+		$("#shopping-cart-results").html('<img src="images/ajax-loader.gif">'); //show loading image
+		$("#shopping-cart-results" ).load( "cart_process.php", {"load_cart":"1"}); //Make ajax request using jQuery Load() & update results
+	});
+
+	//Close Cart
+	$( ".close-shopping-cart-box").click(function(e){ //user click on cart box close link
+		e.preventDefault();
+		$(".shopping-cart-box").fadeOut(); //close cart-box
+	});
+
+	//Remove items from cart
+	$("#shopping-cart-results").on('click', 'a.remove-item', function(e) {
+		e.preventDefault();
+		var pcode = $(this).attr("data-code"); //get product code
+		$(this).parent().fadeOut(); //remove item element from box
+		$.getJSON( "cart_process.php", {"remove_code":pcode} , function(data){ //get Item count from Server
+			$("#cart-info").html(data.items); //update Item count in cart-info
+			$(".cart-box").trigger( "click" ); //trigger click on cart-box to update the items list
+		});
+	});
+
+});
+</script>
+</head>
+
+
+<body>
+<div align="center">
+<h3>YO YO Ma's House of YO-YO's</h3>
+</div>
+
+
+
+<a href="#"  class="cart-box" id="cart-info" title="View Cart">
+<?php
+if(isset($_SESSION["products"])){
+	echo count($_SESSION["products"]);
+}else{
+	echo 0;
+}
+?>
+</a>
+
+<div class="shopping-cart-box" >
+<a href="#" class="close-shopping-cart-box" >Close</a>
+<h3>Your Shopping Cart</h3>
+    <div id="shopping-cart-results" >
+    </div>
+</div>
 
 
 
@@ -113,8 +183,5 @@ $item .= '</ul></div>';
 
 echo $item;
 ?>
-
-<?php include 'footer.php'; ?>
-
-
-
+ </body>
+</html>
