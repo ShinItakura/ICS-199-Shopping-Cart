a83a02045ef81c69267f785becd18ee00bd6b62b
Paul Sajna <paulsajna@gmail.com>
2018-06-04 15:43:49 -0700

    add nav/header/footer to index.php

--- header.php
+++ header.php
@@ -12,10 +12,10 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="style/style.css">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="cart.css">
-
 
 
 
