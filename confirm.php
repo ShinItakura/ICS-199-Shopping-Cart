d86b832d30dc331ee77cb97688f9210a8325a58f
shin <shinitakura@Shins-MacBook-Air.local>
2018-06-04 14:43:28 -0700

    made text file name unique using id firstname lastname and date

--- confirm.php
+++ confirm.php
@@ -1,20 +1,20 @@
 <?php
 ini_set('display_errors',1);
 
-inclued ('mysqli_connect.php');
+include ('mysqli_connect.php');
 // on checkout click send order data to a text file
 
 // database query for order goes here
-$q = "SELECT * FROM USER;";
+$q = "SELECT id,fname,lname,orderDate FROM USER u, ORDER o;";
 
 // Get info from data base while loop
 $r = mysqli_query ($dbc, $q);
 while($row = mysqli_fetch_array ($r, MYSQLI_ASSOC)){
     //open file and write
-    $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-    $txt = $q;
-    fwrite($myfile, $txt);
-    $txt = "Jane Doe\n";
+    $myfile = fopen("{$row['id,fname, lname, orderDate']}.txt", "w") or die("Unable to open file!");
+    $orderUser = $q;
+    fwrite($myfile, $orderUser);
+    $txt = "test/n";
     fwrite($myfile, $txt);
     fclose($myfile);
 }
