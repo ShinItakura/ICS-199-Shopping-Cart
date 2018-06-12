9a4a9a9f942e243171eaaa840e529b468b550ed4
Paul Sajna <paulsajna@gmail.com>
2018-06-12 13:57:53 -0700

    fix the links in the footer

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="span12">
                 <p class="pull-left">Copyright &copy; 2018 Yo Yo Ma, Inc. All Right Reserved</p>
-                <p class="pull-right"><a href="user-agreement.html">Privacy Policy</a> | <a href="user-agreement.html">Terms of Use</a></p>
+                <p class="pull-right"><a href="agreement.php">Privacy Policy</a> | <a href="agreement.php">Terms of Use</a></p>
             </div>
         </div>
     </div>
