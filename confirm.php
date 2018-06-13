b6beeee7accc27f0abe5cbcf9e02cafe5298bf58
shin <shinitakura@Shins-MacBook-Air.local>
2018-06-12 21:01:28 -0700

    convert to string not working yet

--- confirm.php
+++ confirm.php
@@ -5,22 +5,25 @@ include ('mysqli_connect.php');
 // on checkout click send order data to a text file
 
 // database query for order goes here
-$q = "SELECT fname,lname, email, u.id, i.name from ICS199Group13_dev.USER u, ICS199Group13_dev.PURCHASE p, ICS199Group13_dev.ITEM i WHERE u.id = p.USER_id and email = '{$_POST['email']}}';";
+/*
+$q = "SELECT u.id, fname, lname, email, i.name FROM USER u, PURCHASE p, ITEM i WHERE u.id = p.USER_id and email = 'dgreening@camosun.bc.ca';";
+*/
+$q = "SELECT * FROM USER;";
 
-// Get info from data base while loop
-$r = mysqli_query ($dbc, $q);
-while($row = mysqli_fetch_array ($r, MYSQLI_ASSOC)){
-    //open file and write
-    $myfile = fopen("{$row['id,fname, lname, orderDate']}.txt", "w") or die("Unable to open file!");
-    // user email
-    $email = {$row['email']};
-    fwrite($myfile, $email);
-    $orderUser = $q;
-    fwrite($myfile, $orderUser);
-    $txt = "Thank you for your purchase we are processing your order, and we will ship your order in the next few days. Due to excess orders during this time please be patient about with status of your order. We will send you a tracking number as soon as shipping commences.";
-    fwrite($myfile, $txt);
-    fclose($myfile);
+// root directory where file should be created and stored
+$file = "/home/student/cst136/myfiles/testreceipt.txt";
+// creates or opens file at location
+$fileHandler = fopen($file, 'w') or die("can't open file");
+
+// Get selected info from database
+$rq = mysqli_query($dbc,$q);
+// Convert sql data to string
+$receiptQuery = strval($rq);
+// Write to text file the receipt data from database
+fwrite($fileHandler, $receiptQuery);
+// Write to text file email message confirmation
+$txt = "Thank you for your purchase we are processing your order, and we will ship your order in the next few days. Due to excess orders during this time please be patient about with status of your order. We will send you a tracking number as soon as shipping commences.";
+fwrite($fileHandler, $txt);
+fclose($fileHandler);
     // recommended by Jon 
-}
-
 ?>
