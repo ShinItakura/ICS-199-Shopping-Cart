9b0943167468057caef251ecb8ddca0987822353
Bob Cook <ender1117@yahoo.com>
2018-06-13 13:44:12 -0700

    header

--- header.php
+++ header.php
@@ -25,7 +25,40 @@
 <script src="js/login-register.js" type="text/javascript"></script>
 <link rel="stylesheet" href="style/style.css">
 
-
+<style>
+	div.d {
+		position: relative;
+		width: 300;
+		left: 150px;
+		top: 10px;
+		height: 175px;
+		
+	}
+	div.e {
+			position: relative;
+			width: 50;
+			left: 0px;
+			height: 22px;
+			background-color:rgba(128, 128, 128, 1.0);
+		    
+		}
+	p.e
+		  {
+			display:inline;  
+			position: relative; 
+			left:145px;
+			color:#ffffff
+	}
+	p.f
+		  {
+			display:inline;  
+			position: relative; 
+			left:145px;
+			color:#cce6ff; // blue
+	}
+		 
+	 
+	</style>
 
 
 </head>
@@ -36,11 +69,10 @@
  
 
 	<!-- container -->
-	<div class="container">
-		<div class="row">
-		<img src="images/YoYo_LOGO_4.jpg">			
-       </div>
-	   <div class="d">
+	<div class="d">
+		
+	   <img src="images/YoYo_LOGO_4.jpg"></div>
+	   <div class="e">
 	   <?php
 	   include_once(mysqli_connect.php);
 	   //ini_set('display_errors',1);
@@ -51,7 +83,7 @@
 		$username = $test['fname'];
 		$username2 = $test['lname'];
 		$userrole = $test['role'];
-		echo "<p> Welcome $userrole $username $username2</p>";	
+		echo "<p class='f'> Welcome $userrole $username $username2</p>";	
         ?>
 	   </div>
 	   
