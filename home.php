82049f16660025b994b4b8118c138ba105b04fee
Ken <kb_rw@hotmail.com>
2018-06-21 15:35:44 -0700

    Product details now displays all stored data

--- home.php
+++ home.php
@@ -9,7 +9,7 @@ include('header.php');
 <div class="agreement"><span>
     <h1>Welcome to Yo Yo Ma's House of Yo-Yo's!</h1>
 	<p class="d">We've been not selling Yo-Yo's for over six weeks.</p>
-	<p class="d">Physical locations are a government construct to maintain control over its citizens, so don't bother trying to find us.</p>
+	<p class="d">Physical locations are a government construct to maintain control over its citizens, so don't bother trying to find us when you want to complain.</p>
 	<p class="d">The world is not round. It in fact has the shape of a Classic Yo-Yo, with its string attached to the bow that plays the Almighty Cello of the universe.</p>
 
 <?php
